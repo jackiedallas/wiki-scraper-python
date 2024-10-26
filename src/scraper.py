6abb8549ef20ae45546c2e5e9979764e7280da1f
Jackie Johnson-Dallas <j.johnso-dallas@afs.com>
2024-10-25 21:15:37 -0400

    added code to get header from wikipedia

---
+++ src/scraper.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup # type: ignore
+
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/Web_scraping",
+)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find(id="firstHeading")
+print(title.string)
