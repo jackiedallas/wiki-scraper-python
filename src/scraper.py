cf09d48e2f4dec16d6e921cfac25c0ae2878a630
Jackie Johnson-Dallas <j.johnso-dallas@afs.com>
2024-10-25 21:35:31 -0400

    put code into a function

--- src/scraper.py
+++ src/scraper.py
@@ -8,9 +8,34 @@ response = requests.get(
     url="https://en.wikipedia.org/wiki/Web_scraping",
 )
 
-# Initialize parser
-soup = BeautifulSoup(response.content, 'html.parser')
+# Function for scraping
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url,
+    )
 
-# Query for the Header and print the String
-title = soup.find(id="firstHeading")
-print(title.string)
+    # Initialize parser
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Query for the Header and print the String
+    title = soup.find(id="firstHeading")
+    print(title.text)
+
+    # Get all links on page
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
+
+    # Loop through all the links on the page and only get the Wiki Articles
+    for link in allLinks:
+        if link['href'].find("/wiki/") == -1:
+            continue
+
+        # Get a random wiki link
+        linkToScrape = link
+        break
+
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+# Call function
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraping")
