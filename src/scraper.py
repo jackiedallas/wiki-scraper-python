2249ce7d11910614d6455151c9f16236687fbcb1
Jackie Johnson-Dallas <j.johnso-dallas@afs.com>
2024-10-25 21:18:38 -0400

    added comments

--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,16 @@
+# Import modules
 import requests
 from bs4 import BeautifulSoup # type: ignore
+import random
 
+# Get request to URL
 response = requests.get(
     url="https://en.wikipedia.org/wiki/Web_scraping",
 )
+
+# Initialize parser
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# Query for the Header and print the String
 title = soup.find(id="firstHeading")
 print(title.string)
